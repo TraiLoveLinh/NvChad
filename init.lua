faba6f2c4fcceeac60a77f1bc36c4d1667c725e6
TraiLoveLinh <traipupgmobile@gmail.com>
2024-08-14 21:45:43 +0700

    add f5 and f6 keys build and run feature for c++ file

--- init.lua
+++ init.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
     end,
   },
 
-  { import = "plugins" },
+  -- { import = "plugins" },
 }, lazy_config)
 
 -- load theme
@@ -37,3 +37,21 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+
+-- Bind F6 to save and run the compiled file in Alacritty, waiting for Enter key to continue
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "cpp",
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, "n", "<F6>", ":w<CR>:!alacritty -e sh -c './" .. vim.fn.expand("%:r") .. " && read -p \"Press enter to continue\"'<CR>", { noremap = true, silent = true })
+    end,
+})
+
+-- Bind F5 to save, compile with g++, and run the file in Alacritty, waiting for Enter key to continue
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "cpp",
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, "n", "<F5>", ":w<CR>:!g++ -o " .. vim.fn.expand("%:r") .. " " .. vim.fn.expand("%") .. " && alacritty -e sh -c './" .. vim.fn.expand("%:r") .. " && read -p \"Press enter to continue\"'<CR>", { noremap = true, silent = true })
+    end,
+})
+
---
+++ plugins/bro.lua
@@ -0,0 +1,5 @@
+local plugins = {
+    { "neoclide/coc.nvim" , lazy = false }
+}
+
+return plugins
